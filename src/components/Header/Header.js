7a2708da4c5506be7d63c8e6c457c7ba57b7c435
Josh Simmons <joshsimmonsjosh@gmail.com>
2018-09-25 21:02:43 -0400

    Add picture of server

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,9 +6,9 @@ import React, { Component } from 'react';
 export class Header extends Component {
     render() {
         return (
-            <div class="jumbotron jumbotron-fluid" style={{display:'flex', justifyContent:'space-around', flexDirection:'column', backgroundImage:'url(./assets/plane.jpg)'}}  >
+            <div class="jumbotron jumbotron-fluid" style={{display:'flex', justifyContent:'space-around', flexDirection:'column', backgroundImage:'url(./assets/servers.jpg)'}}  >
                 <div class="container">
-                    <h1 style={{flex:1}}  class="display-4">BornTec</h1>
+                    <h1 style={{flex:1}}  class="display-4 text-white">BornTec</h1>
                 </div>
             </div>
         );
