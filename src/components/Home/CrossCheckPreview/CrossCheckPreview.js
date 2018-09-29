6c3a71e3265e61faca98bb1efc00064fab50a22a
Josh Simmons <joshsimmonsjosh@gmail.com>
2018-09-28 22:22:11 -0400

    Add icon for cross check preview

--- src/components/Home/CrossCheckPreview/CrossCheckPreview.js
+++ src/components/Home/CrossCheckPreview/CrossCheckPreview.js
@@ -5,15 +5,16 @@ import React, { Component } from 'react';
 class CrossCheckPreview extends Component {
     render() {
         return (
-            <div className="card" style={{width: '18rem', margin:'10px', display:'inline-block'}}>
-                <img className="card-img-top" src=".../100px180/" alt="Card image cap" />
+            <div className="card" style={{ backgroundColor: 'rgb(30, 30, 30)', width: '18rem', margin: '10px', display: 'inline-block' }}>
+                <div className='bg-dark' style={{display:'flex', justifyContent:'space-around', alignItems:'center', padding:'25px', height:'120px', width:'120px', borderRadius:'50%'}}>
+                    <i style={{ fontSize: '100px', color: 'coral' }} class="fas fa-arrow-right"></i>
+                </div>
                 <div className="card-body">
-                    <h5 className="card-title" align="center">Cross Check</h5>
-                    <p className="card-text" align="center">Compliance and Monitoring System, MiFID II, CFTC and MAR</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <h5 className="card-title text-light" align="center">Cross Check</h5>
+                    <p className="card-text text-light" align="center">Compliance and Monitoring System, MiFID II, CFTC and MAR</p>
                 </div>
                 <br />
-            </div>
+            </div >
         );
     }
 }
