8c8bd878beb17c03f7a9cd9ba879d084b3b42920
Josh Simmons <joshsimmonsjosh@gmail.com>
2018-09-28 21:45:35 -0400

    Fix merge conflict

--- src/components/Home/MainPageHeader/MainPageHeader.js
+++ src/components/Home/MainPageHeader/MainPageHeader.js
@@ -7,11 +7,7 @@ import Header from '../../Header/Header'
 class MainPageHeader extends Component {
     render() {
         return (
-<<<<<<< HEAD
             <div id='main-page-header' className="container-fluid jumbotron-fluid" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexWrap:'wrap', 'alignContent':'center', background:'url(./assets/servers.jpg) no-repeat center center',backgroundSize:'cover' }}  >
-=======
-            <div id='main-page-header' className="container-fluid jumbotron-fluid" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexWrap:'wrap', 'alignContent':'center', backgroundImage:'url(./assets/servers.jpg)', color: "coral" }}  >
->>>>>>> 23c7e3074550a920f38ab204a272f1f739eec9ee
                 <h1 id="main-tag" style={{ fontFamily: 'Maven Pro' }} className="display-5 text-white">Smarter Together</h1>
                 <h4 id="supporting-tag" style={{ flexBasis:'100%', marginLeft:'1.5em', marginRight:'1.5em',  textAlign:'center', color: "coral", fontFamily: 'Maven Pro' }} className="text-white">this should say something nice</h4>
             </div>
