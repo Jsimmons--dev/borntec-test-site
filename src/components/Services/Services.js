1e39d9cc7284e08bac060713c989b6a37c245bf1
Christopher Boyle <chrisboyle1993@gmail.com>
2018-10-01 20:34:01 -0400

    services page update

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -13,26 +13,26 @@ export class Services extends Component {
                     <div class="container">
                         <h1 class="display-4" style={{color:'DodgerBlue'}}>Managed Hosting Services</h1>
                         <h3 class="display-4" style={{color: 'White'}}>Trading Technologies</h3>
-                        <p class="lead">An extremely popular trading tool with extensive customization capabilities. Proprietary trading groups, locals and hedge funds are large users of X_Trader.
+                        <p class="lead" style={{color:"white"}}>An extremely popular trading tool with extensive customization capabilities. Proprietary trading groups, locals and hedge funds are large users of X_Trader.
                         </p>
                     </div>
                 </div>
                 <div class="jumbotron jumbotron-fluid">
                     <div class="container">
                         <h1 class="display-4" style={{color:'Coral'}}>Direct Market Access</h1>
-                        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                        <p class="lead" style={{color:"white"}}>This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
                     </div>
                 </div>
                 <div class="jumbotron jumbotron-fluid">
                     <div class="container">
                         <h1 class="display-4" style={{color:'DodgerBlue'}}>Low Latency Trading</h1>
-                        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                        <p class="lead" style={{color:"white"}}>This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
                     </div>
                 </div>
                 <div class="jumbotron jumbotron-fluid">
                     <div class="container">
                         <h1 class="display-4">Fluid jumbotron</h1>
-                        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                        <p class="lead" style={{color:"white"}}>This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
                     </div>
                 </div>
             </div >
