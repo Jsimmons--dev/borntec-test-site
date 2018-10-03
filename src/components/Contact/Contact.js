fb4de92b64ab3c9aa4410a3d8d9c93982e154cbe
Josh Simmons <joshsimmonsjosh@gmail.com>
2018-10-02 21:51:09 -0400

    Add calandly but commented out pending styling options

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,24 +4,34 @@ import './Contact.css';
 class Contact extends Component {
     render() {
         return (
-            <div style={{display:'flex', alignItems:'center', justifyContent:'space-around', flexDirection:'column', height:"800px"}} id='contact'>
+            <div style={{display:'flex', justifyContent:'space-around'}}>
+                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around', flexDirection: 'column', height: "800px" }} id='contact'>
+                </div>
             </div>
+
         );
     }
+
 
     componentDidMount() {
         window.hbspt.forms.create({
             portalId: "4754439",
             formId: "d1971e99-049b-4960-ac18-a31f5afae4a4",
             target: '#contact',
-            onFormReady: function(form) {
+            onFormReady: function (form) {
                 //form.parent().addClass('borntec-contact')
                 let formFrame = window.$('.hs-form-iframe')
                 formFrame.addClass('borntec-contact')
                 formFrame.css('width', '')
             }
         })
-        
+
+        // const s = document.createElement('script');
+        // s.type = 'text/javascript';
+        // s.async = true;
+        // s.innerHTML = "Calendly.initBadgeWidget({url: 'https://calendly.com/chrisboyle1993', text: 'Schedule time with me', color: '#4d5055', branding: true});";
+        // this.instance.appendChild(s);
+
     }
 }
 
