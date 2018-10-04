0118c6b2bc2bdff8b8f61821ff0952607a3fd429
Christopher Boyle <chrisboyle1993@gmail.com>
2018-10-04 19:36:58 -0400

    merge change

--- src/components/Solutions/Solutions.js
+++ src/components/Solutions/Solutions.js
@@ -17,47 +17,6 @@ export class Solutions extends Component {
                 <div id='main-page-header' className="container-fluid jumbotron-fluid" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexWrap: 'wrap', 'alignContent': 'center', background: 'url(./assets/plane.jpg) no-repeat center center' }}  >
                     <h1 id="main-tag" style={{ fontFamily: 'Maven Pro' }} className="display-5">MEET OUR SOLUTIONS</h1>
                 </div>
-<<<<<<< HEAD
-                <div style={{ backgroundColor: "rgb(30, 30, 30)" }} class="jumbotron jumbotron-fluid">
-                    <div class="container">
-                        <h1 align='Left' class="display-4" style={{color:"dodgerblue"}}>Cross Check</h1>
-                        <p align='Left' style={{color:"white"}}>Compliance and Monitoring System, MiFID II, CFTC and MAR</p>
-                    </div>
-                    <hr />
-                    <div className={'container-fluid'} style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
-                        <div>
-                            <h5 style={{color: "dodgerblue"}}>Automated Trading and DEA Management</h5>
-                            <ul>
-                                <li style={{color: "White"}}>Monitor messaging velocity in realtime</li>
-                                <li style={{color: "White"}}>Visualize by exchange, trade session, user, algo ID & more</li>
-                                <li style={{color: "White"}}>Monitor for rejections and limit band breaches</li>
-                                <li style={{color: "White"}}>Realtime alerts via email, SMS, Symphony, Slack, etc.</li>
-                            </ul>
-                        </div>
-                        <div>
-                            <h5 style={{color: "dodgerblue"}}>Position Monitoring and Alerting</h5>
-                            <ul>
-                                <li style={{color: "White"}}>Aggregate working orders and filled positions from all trading systems and exchanges in realtime</li>
-                                <li style={{color: "White"}}>Group blocks of accounts into custom risk groups</li>
-                                <li style={{color: "White"}}>Define thresholds for clip size, limit breaches, and many more parameters</li>
-                            </ul>
-                        </div>
-                        <div>
-                            <h5 style={{color: "dodgerblue"}}>Transaction Reporting for MiFID II</h5>
-                            <ul>
-                                <li style={{color: "White"}}>Automated retrieval of customer trade records</li>
-                                <li style={{color: "White"}}>Data enrichment and formatting</li>
-                                <li style={{color: "White"}}>Report validation</li>
-                                <li style={{color: "White"}}>Secure delivery to regulators</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div style={{ backgroundColor: "rgb(30, 30, 30" }} class="jumbotron jumbotron-fluid">
-                    <div class="container">
-                        <h1 align='Left' class="display-4" style={{color:"Dodgerblue"}}>TraC</h1>
-                        <p align='Left' style={{color:"white"}}>Real-Time Trade Blotter Across All Vendors/Exchanges</p>
-=======
                 <div className='container'>
                     <div id='crosscheck' style={{ backgroundColor: "rgb(30, 30, 30)" }} class="jumbotron jumbotron-fluid">
                         <div align='center' class="container">
@@ -95,7 +54,6 @@ export class Solutions extends Component {
                                 </div>
                             </div>
                         </div>
->>>>>>> b72361d8ac48e485f4e752acd2973de729a4d57f
                     </div>
                     <div id='trac' style={{ backgroundColor: "rgb(30, 30, 30" }} class="jumbotron jumbotron-fluid">
                         <div align='center' class="container">
@@ -131,34 +89,6 @@ export class Solutions extends Component {
                                 </div>
                             </div>
                         </div>
-<<<<<<< HEAD
-                        <div >
-                            <h5 style={{color:'dodgerblue'}}>Automated Trading and DEA Management</h5>
-                            <ul>
-                                <li style={{color: "White"}}>Monitor messaging velocity in realtime</li>
-                                <li style={{color: "White"}}>Visualize by exchange, trade session, user, algo ID & more</li>
-                                <li style={{color: "White"}}>Monitor for rejections and limit band breaches</li>
-                                <li style={{color: "White"}}>Realtime alerts via email, SMS, Symphony, Slack, etc.</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div style={{ backgroundColor: "rgb(30, 30, 30" }} class="jumbotron jumbotron-fluid">
-                    <div class="container">
-                        <h1 align='Left' class="display-4" style={{color:'dodgerblue'}}>CORE</h1>
-                        <p align='Left' style={{color:'White'}}>Care Order STP Between Clients and Brokers</p>
-                    </div>
-                    <hr />
-                    <div className={'container-fluid'} style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
-                        <div>
-                            <h5 style={{color:'dodgerblue'}}>Client-Broker FIX Routing for Care Orders</h5>
-                            <ul>
-                                <li style={{color: "White"}}>SaaS deployment</li>
-                                <li style={{color: "White"}}> Full STP for clients</li>
-                                <li style={{color: "White"}}>Brokers execute away or via DMA</li>
-                                <li style={{color: "White"}}> Many OMS’s pre-certified</li>
-                            </ul>
-=======
                     </div>
                     <div id='core' style={{ backgroundColor: "rgb(30, 30, 30" }} class="jumbotron jumbotron-fluid">
                         <div align='center' class="container">
@@ -178,7 +108,6 @@ export class Solutions extends Component {
                                     </ul>
                                 </div>
                             </div>
->>>>>>> b72361d8ac48e485f4e752acd2973de729a4d57f
                         </div>
                     </div>
                     <div id='jetconnect' style={{ backgroundColor: "rgb(30, 30, 30" }} class="jumbotron jumbotron-fluid">
