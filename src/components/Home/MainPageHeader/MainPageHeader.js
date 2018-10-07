dce73de012a3c1364f3ea2ae3ae6cb453909c277
Christopher Boyle <chrisboyle1993@gmail.com>
2018-10-07 11:42:25 -0400

    header content lowercase

--- src/components/Home/MainPageHeader/MainPageHeader.js
+++ src/components/Home/MainPageHeader/MainPageHeader.js
@@ -6,7 +6,7 @@ class MainPageHeader extends Component {
     render() {
         return (
             <div id='main-page-header' className="container-fluid jumbotron-fluid" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexWrap: 'wrap', 'alignContent': 'center', background: 'url(./assets/servers.jpg) no-repeat center center', backgroundSize: 'cover' }}  >
-                <h1 id="main-tag" style={{ fontFamily: 'Maven Pro' }} className="display-5">SMARTER TOGETHER</h1>
+                <h1 id="main-tag" style={{ fontFamily: 'Maven Pro' }} className="display-5">Smarter Together</h1>
                 <h4 id="supporting-tag" style={{ flexBasis: '100%', marginLeft: '1.5em', marginRight: '1.5em', textAlign: 'center', fontFamily: 'Maven Pro' }}>Delivering <strong style={{color:'dodgerblue'}}>tools</strong> and <strong style={{color:'dodgerblue'}}>technologies</strong> to <strong style={{color:'dodgerblue'}}>accelerate</strong> change in <strong style={{color:'dodgerblue'}}>today's marketplace</strong></h4>
             </div>
         );
--- src/components/Solutions/Solutions.js
+++ src/components/Solutions/Solutions.js
@@ -15,7 +15,7 @@ export class Solutions extends Component {
         return (
             <div >
                 <div id='main-page-header' className="container-fluid jumbotron-fluid" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexWrap: 'wrap', 'alignContent': 'center', background: 'url(./assets/chopper.jpg) no-repeat center center', backgroundSize: 'cover' }}  >
-                <h1 id="main-tag" style={{ fontFamily: 'Maven Pro' }} className="display-5">MEET OUR SOLUTIONS</h1>
+                <h1 id="main-tag" style={{ fontFamily: 'Maven Pro' }} className="display-5">Meet our solutions</h1>
                 <h4 id="supporting-tag" style={{ flexBasis: '100%', marginLeft: '1.5em', marginRight: '1.5em', textAlign: 'center', fontFamily: 'Maven Pro' }}>Explore our <strong style={{color:'dodgerblue'}}>products</strong> that create our <strong style={{color:'dodgerblue'}}>Flight Control</strong> suite</h4>
             </div>
                 <div className='container'>
