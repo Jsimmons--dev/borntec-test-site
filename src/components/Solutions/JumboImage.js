af90eb2abcae5bb8234678a26e3b188276351581
Josh Simmons <joshsimmonsjosh@gmail.com>
2018-10-08 23:21:36 -0400

    Added images to solutions page

---
+++ src/components/Solutions/JumboImage.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { theme } from '../../constants'
+
+class JumboImage extends Component {
+
+    render() {
+        console.log('reverse?', this.props.reverse, 'container ' + (this.props.reverse === true ? 'jumbo-image-reverse' : 'jumbo-image'))
+        return (
+            <div id={this.props.id} style={{ backgroundColor: "rgb(30, 30, 30" }} class="jumbotron jumbotron-fluid">
+                <div align='center' class="container">
+                    <h1 class="display-4" style={{ color: theme.primaryColor1 }}>{this.props.title}</h1>
+                    <p>{this.props.desc}</p>
+                </div>
+                <hr />
+                <div className={(this.props.reverse === true ? 'jumbo-image-reverse container-fluid' : 'jumbo-image container')} style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
+                    <div style={{ flex: '1', padding: '20px' }}>
+                        <div>
+
+                            <ul>
+                                {
+                                    this.props.list.map(list => {
+                                        return list.map((li, i) => i === 0 ? (<h5 style={{ color: theme.primaryColor1 }}>{li}</h5>) :
+                                            (<li>{li}</li>))
+                                    })
+
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                    <div style={{ flex: '1', minHeight: '300px', backgroundImage: `url("${this.props.image}")`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}></div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default JumboImage
--- src/components/Solutions/Solutions.js
+++ src/components/Solutions/Solutions.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from '../Header/Header'
 import './Solutions.css'
 import { theme } from '../../constants'
+import JumboImage from './JumboImage'
 
 
 export class Solutions extends Component {
@@ -15,133 +16,50 @@ export class Solutions extends Component {
         return (
             <div >
                 <div id='main-page-header' className="container-fluid jumbotron-fluid" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexWrap: 'wrap', 'alignContent': 'center', background: 'url(./assets/stockpad.jpg) no-repeat center center', backgroundSize: 'cover' }}  >
-                <h1 id="main-tag" style={{ fontFamily: 'Maven Pro' }} className="display-5">Meet our solutions</h1>
-                <h4 id="supporting-tag" style={{ flexBasis: '100%', marginLeft: '1.5em', marginRight: '1.5em', textAlign: 'center', fontFamily: 'Maven Pro' }}>Explore our <strong style={{color:'dodgerblue'}}>products</strong> that create our <strong style={{color:'dodgerblue'}}>Flight Control</strong> suite</h4>
-            </div>
-                <div className='container'>
-                    <div id='crosscheck' style={{ backgroundColor: "rgb(30, 30, 30)" }} class="jumbotron jumbotron-fluid">
-                        <div align='center' class="container">
-                            <h1 class="display-4" style={{ color: theme.primaryColor1 }}>Cross Check</h1>
-                            <p>Compliance and Monitoring System, MiFID II, CFTC and MAR</p>
-                        </div>
-                        <hr />
-                        <div className={'container-fluid'} style={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap', justifyContent: 'space-around' }}>
-                            <div>
-                                <div>
-                                    <h5 style={{ color: theme.primaryColor1 }}>Automated Trading and DEA Management</h5>
-                                    <ul>
-                                        <li>Monitor messaging velocity in realtime</li>
-                                        <li>Visualize by exchange, trade session, user, algo ID & more</li>
-                                        <li>Monitor for rejections and limit band breaches</li>
-                                        <li>Realtime alerts via email, SMS, Symphony, Slack, etc.</li>
-                                    </ul>
-                                </div>
-                                <div>
-                                    <h5 style={{ color: theme.primaryColor1 }}>Position Monitoring and Alerting</h5>
-                                    <ul>
-                                        <li>Aggregate working orders and filled positions from all trading systems and exchanges in realtime</li>
-                                        <li>Group blocks of accounts into custom risk groups</li>
-                                        <li>Define thresholds for clip size, limit breaches, and many more parameters</li>
-                                    </ul>
-                                </div>
-                                <div>
-                                    <h5 style={{ color: theme.primaryColor1 }}>Transaction Reporting for MiFID II</h5>
-                                    <ul>
-                                        <li>Automated retrieval of customer trade records</li>
-                                        <li>Data enrichment and formatting</li>
-                                        <li>Report validation</li>
-                                        <li>Secure delivery to regulators</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div id='trac' style={{ backgroundColor: "rgb(30, 30, 30" }} class="jumbotron jumbotron-fluid">
-                        <div align='center' class="container">
-                            <h1 class="display-4" style={{ color: theme.primaryColor2 }}>TraC</h1>
-                            <p>Real-Time Trade Blotter Across All Vendors/Exchanges</p>
-                        </div>
-                        <hr />
-                        <div className={'container-fluid'} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end', flexWrap: 'wrap', justifyContent: 'space-around' }}>
-                            <div>
-                                <div>
-                                    <h5 style={{ color: theme.primaryColor2 }}>Cost Effective Terminals for Risk & Compliance</h5>
-                                    <ul>
-                                        <li >TraC is intuitive and easy to use for non-trading personnel</li>
-                                    </ul>
-                                </div>
-                                <div>
-                                    <h5 style={{ color: theme.primaryColor2 }}>Single Screen, Rapid Access to All Order Flow</h5>
-                                    <ul>
-                                        <li>Navigate one application for worldwide order flow</li>
-                                        <li>Easily search orders by any field</li>
-                                        <li>View and cancel orders</li>
-                                        <li>Export and download order flow detail</li>
-                                    </ul>
-                                </div>
-                                <div >
-                                    <h5 style={{ color: theme.primaryColor2 }}>Automated Trading and DEA Management</h5>
-                                    <ul>
-                                        <li>Monitor messaging velocity in realtime</li>
-                                        <li>Visualize by exchange, trade session, user, algo ID & more</li>
-                                        <li>Monitor for rejections and limit band breaches</li>
-                                        <li>Realtime alerts via email, SMS, Symphony, Slack, etc.</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div id='core' style={{ backgroundColor: "rgb(30, 30, 30" }} class="jumbotron jumbotron-fluid">
-                        <div align='center' class="container">
-                            <h1 class="display-4" style={{ color: theme.primaryColor1 }}>CORE</h1>
-                            <p>Care Order STP Between Clients and Brokers</p>
-                        </div>
-                        <hr />
-                        <div className={'container-fluid'} style={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap', justifyContent: 'space-around' }}>
-                            <div>
-                                <div>
-                                    <h5 style={{ color: theme.primaryColor1 }}>Client-Broker FIX Routing for Care Orders</h5>
-                                    <ul>
-                                        <li>SaaS deployment</li>
-                                        <li> Full STP for clients</li>
-                                        <li>Brokers execute away or via DMA</li>
-                                        <li> Many OMS’s pre-certified</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div id='jetconnect' style={{ backgroundColor: "rgb(30, 30, 30" }} class="jumbotron jumbotron-fluid">
-                        <div align='center' class="container">
-                            <h1 class="display-4" style={{ color: theme.primaryColor2 }}>JetConnect</h1>
-                            <p>SaaS Real-Time Trade Flow Distribution</p>
-                        </div>
-                        <hr />
-                        <div className={'container-fluid'} style={{ display: 'flex', alignItems: 'flex-end', flexDirection: 'column', flexWrap: 'wrap', justifyContent: 'space-around' }}>
-                            <div>
-                                <div>
-                                    <h5 style={{ color: theme.primaryColor2 }}>Flexibility</h5>
-                                    <ul>
-                                        <li>Production ready in days to weeks</li>
-                                        <li>Multi-Asset, Multi-Market Trade Flow</li>
-                                        <li>Easy counterparty connectivity</li>
-                                        <li>30+ API’s plug-and-play</li>
-                                    </ul>
-                                </div>
-                                <div>
-                                    <h5 style={{ color: theme.primaryColor2 }}>Consolidation</h5>
-                                    <ul>
-                                        <li>Rapid DMA/FIX Integrations with easy normalization and transformation</li>
-                                        <li>Central hub for all trade flow</li>
-                                        <li>Trade reporting in flexible formats via email/sftp</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <h1 id="main-tag" style={{ fontFamily: 'Maven Pro' }} className="display-5">Meet our solutions</h1>
+                    <h4 id="supporting-tag" style={{ flexBasis: '100%', marginLeft: '1.5em', marginRight: '1.5em', textAlign: 'center', fontFamily: 'Maven Pro' }}>Explore our <strong style={{ color: 'dodgerblue' }}>products</strong> that create our <strong style={{ color: 'dodgerblue' }}>Flight Control</strong> suite</h4>
+                </div>
+                <div>
+
+                    <JumboImage title={'Cross Check'}
+                        id={'crosscheck'}
+                        desc={'Compliance and Monitoring System, MiFID II, CFTC and MAR'}
+                        list={[
+                            ['Automated Trading and DEA Management', 'Monitor messaging velocity in realtime', 'Visualize by exchange, trade session, user, algo ID & more', 'Monitor for rejections and limit band breaches', 'Realtime alerts via email, SMS, Symphony, Slack, etc.'],
+                            ['Position Monitoring and Alerting', 'Aggregate working orders and filled positions from all trading systems and exchanges in realtime', 'Group blocks of accounts into custom risk groups', 'Define thresholds for clip size, limit breaches, and many more parameters'],
+                            ['Transaction Reporting for MiFID II', 'Automated retrieval of customer trade records', 'Data enrichment and formatting', 'Report validation', 'Secure delivery to regulators']
+                        ]}
+                        image={'./assets/crosscheck_feature.jpg'} />
+
+                    <JumboImage title={'TraC'}
+                        reverse={true}
+                        id={'trac'}
+                        desc={'Real-Time Trade Blotter Across All Vendors/Exchanges'}
+                        list={[
+                            ['Cost Effective Terminals for Risk & Compliance', 'TraC is intuitive and easy to use for non-trading personnel'],
+                            ['Single Screen, Rapid Access to All Order Flow', 'Navigate one application for worldwide order flow', 'Easily search orders by any field', 'View and cancel orders', 'Export and download order flow detail'],
+                            ['Automated Trading and DEA Management', 'Monitor messaging velocity in realtime', 'Visualize by exchange, trade session, user, algo ID & more', 'Monitor for rejections and limit band breaches', 'Realtime alerts via email, SMS, Symphony, Slack, etc.']
+                        ]}
+                        image={'./assets/crosscheck_feature.jpg'} />
+
+                    <JumboImage title={'CORE'}
+                        id={'core'}
+                        desc={'Care Order STP Between Clients and Brokers'}
+                        listTop={'Client-Broker FIX Routing for Care Orders'}
+                        list={[['Client-Broker FIX Routing for Care Orders', 'SaaS deployment', 'Full STP for clients', 'Brokers execute away or via DMA', 'Many OMS’s pre-certified']]}
+                        image={'./assets/core_feature.jpg'} />
+
+                    <JumboImage title={'JetConnect'}
+                        reverse={true}
+                        id={'jetconnect'}
+                        desc={'SaaS Real-Time Trade Flow Distribution'}
+                        list={[
+                            ['Flexibility', 'Production ready in days to weeks', 'Multi-Asset, Multi-Market Trade Flow', 'Easy counterparty connectivity', '30+ API’s plug-and-play'],
+                            ['Consolidation', 'Rapid DMA/FIX Integrations with easy normalization and transformation', 'Central hub for all trade flow', 'Trade reporting in flexible formats via email/sftp'],
+                        ]}
+                        image={'./assets/crosscheck_feature.jpg'} />
                 </div>
             </div >
-
         );
     }
 }
