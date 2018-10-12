6b28272811993ca41d3d8a6ec4ce1d0f00426180
Josh Simmons <joshsimmonsjosh@gmail.com>
2018-10-12 08:59:58 -0400

    Center text on contact header

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,7 +7,7 @@ class Contact extends Component {
         return (
             <div>
                 <div id='main-page-header' className="container-fluid jumbotron-fluid" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexWrap: 'wrap', 'alignContent': 'center', background: 'url(./assets/comms.jpg) no-repeat center center', backgroundSize: 'cover' }}  >
-                    <h1 id="main-tag" style={{ fontFamily: 'Maven Pro' }} className="display-5"><strong>Thanks for visiting <strong>BornTec</strong> </strong></h1>
+                    <h1 align='center' id="main-tag" style={{ fontFamily: 'Maven Pro' }} className="display-5"><strong>Thanks for visiting <strong>BornTec</strong> </strong></h1>
                     <h4 id="supporting-tag" style={{ flexBasis: '100%', marginLeft: '1.5em', marginRight: '1.5em', textAlign: 'center', fontFamily: 'Maven Pro' }}><strong>Schedule a time to meet with us or send us your <strong>contact information</strong> and we will <strong>promptly</strong> get back to you!</strong></h4>
                 </div>
                 <br />
