bb89ca379cc8f9e3564a6df5750ee2cd66efa342
Josh Simmons <joshsimmonsjosh@gmail.com>
2018-10-11 21:50:14 -0400

    Style news feed

--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -4,30 +4,32 @@ import { Link } from 'react-router-dom';
 
 class NewsFeed extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {}
     }
     componentDidMount() {
         var wp = new WPAPI({ endpoint: 'http://borntec.com/wp-json' });
         wp.posts().then((data) => {
-            this.setState((state, props) =>  ({ posts:data }))
+            this.setState((state, props) => ({ posts: data }))
         }).catch(function (err) {
             console.log(err)
         });
     }
 
-
     render() {
-        let posts = this.state.posts && this.state.posts.map((d, i) => <li key={i}><Link to={`/article/${d.id}`}>{d.title.rendered}</Link></li>)
+        let posts = this.state.posts && this.state.posts.slice(0,5).map((d, i) => <li style={{margin:'5px'}} key={i}><Link style={{ color: "white" }} to={`/article/${d.id}`} dangerouslySetInnerHTML={{__html:`<i class="fas fa-list-alt"></i> <span style='font-size:20px'>${d.title.rendered}</span> <span style='font-size:12px'> ${new Date(d.date).toDateString()}</span> `}}></Link><hr style={{width:'100%'}}/></li>)
         return (
             <div>
-                <h1 align='center' style={{color:'white'}}> STAY <strong style={{color:'dodgerblue'}}>UP TO DATE</strong> WITH RECENT <strong style={{color:'dodgerblue'}}>NEWS</strong></h1>
-                <br/>
-                <br/>
-                <ul align='center'>
-                    {posts}
-                </ul>
+                <h1 align="center" style={{ color: 'white' }}> STAY <strong style={{ color: 'dodgerblue' }}>UP TO DATE</strong> WITH RECENT <strong style={{ color: 'dodgerblue' }}>NEWS</strong></h1>
+                <br />
+                <br />
+                <div style={{ display: 'flex', justifyContent:'center' }}>
+                    <ul style={{ listStyleType: "none" }}>
+                        {posts}
+                    </ul>
+                </div>
+
             </div >
         );
     }
