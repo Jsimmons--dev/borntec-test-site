48db2979b03179033b1dca1b50c3b23b7c5d5134
Josh Simmons <joshsimmonsjosh@gmail.com>
2018-10-11 22:14:21 -0400

    Add loader to news articles

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,38 +1,52 @@
 import React, { Component } from 'react';
 import * as WPAPI from 'wpapi'
+import './NewsArticle.css'
 
 class NewsArticle extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {}
+        this.progressRef = React.createRef()
     }
     componentDidMount() {
         var wp = new WPAPI({ endpoint: 'http://borntec.com/wp-json' });
         wp.posts().then((data) => {
-            this.setState((state, props) =>  ({ posts:data }))
+            this.setState((state, props) => ({ posts: data }))
         }).catch(function (err) {
             console.log(err)
         });
     }
 
+    componentDidUpdate() {
+        if (this.state.posts) {
+            this.progressRef.current.style.display = 'none'
+        }
+    }
+
 
     render() {
-        console.log(this.props.match.params.id, this.state.posts)
-        let post = {__html:''};
-        if(this.state.posts !== undefined){
+        let post = { __html: '' };
+        if (this.state.posts !== undefined) {
             this.state.posts.forEach(p => {
-                console.log(p)
-                if(`${p.id}` === this.props.match.params.id){
-                    post = {__html:p.content.rendered}
+                if (`${p.id}` === this.props.match.params.id) {
+                    post = { __html: p.content.rendered }
                 }
             })
         }
-        console.log(post)
         return (
-            <div dangerouslySetInnerHTML={post}>
-            </div >
-
+            <div>
+                <div className="container-fluid jumbotron-fluid news-article-header" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexWrap: 'wrap', 'alignContent': 'center', background: 'url(./assets/comms.jpg) no-repeat center center', backgroundSize: 'cover' }}  >
+                    <h1 id="main-tag" style={{ fontFamily: 'Maven Pro' }} className="display-5">BornTec News</h1>
+                </div>
+                <div style={{display:'flex', flexDirection:'column', alignItems:'center'}} ref={this.progressRef} >
+                    <br />
+                    <progress className="pure-material-progress-circular" />
+                    <span style={{fontSize:'30px'}}>Please Wait...</span>
+                </div>
+                <div style={{height:'50vh'}} dangerouslySetInnerHTML={post}>
+                </div >
+            </div>
         );
     }
 }
